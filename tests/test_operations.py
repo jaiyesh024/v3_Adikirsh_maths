d25aa1f25a6824ee06cc012e0ed70ac10a4f6374
jaiyesh024 <jaiyesh024@gmail.com>
2025-06-19 08:06:52 +0100

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,8 @@
-from src.maths_operation import add,sub
+from src.maths_operation import add, sub
 
 def test_add():
-    assert (2,3) == 5
-    
+    assert add(2, 3) == 5
+
 def test_sub():
-    assert (5,3) == 2
+    assert sub(5, 3) == 2
+
